078d73dee7afb25431bebc6b2d8cc80829910dcb
omBagade1 <ombagade951@gmail.com>
2026-02-17 14:00:58 +0530

    hepalo

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,4 @@ app.get("/",(req,res)=>{
    res.json("hello") 
 })
 
-app.listen(process.env.PORT||8000 ,(error)=>{
-   if(error){
-      console.error(error)
-   }
-   else{
-   console.log("Server is listening !!") 
-   }
-})
+
